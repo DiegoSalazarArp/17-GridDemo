f703f598d5dd67369e74353d1e57093e9e86f90b
Diego Salazar Arp <d.salazararp@gmail.com>
2019-08-12 20:22:01 -0400

    Initial Commit

---
+++ 17-GridDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  17-GridDemo
+//
+//  Created by Diego Salazar Arp on 12-08-19.
+//  Copyright © 2019 Diego Salazar Arp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
